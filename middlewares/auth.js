2380135ae059833c60e5b6ae36ffe94a37f7780e
Aman Kumar Singh <singhkumaraman17@gmail.com>
2024-09-28 02:57:08 +0530

    optimized

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,6 @@
 const { getUser } = require("../service/auth");
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-    console.log(req)
   const userUid = req.cookies?.uid;
 
   if (!userUid) return res.redirect("/login");
